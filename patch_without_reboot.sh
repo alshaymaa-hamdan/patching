f861ef912af4509fbf2b25f22e3977bce2992d08
AlShaymaaHamdan <101719699+AlShaymaaHamdan@users.noreply.github.com>
2025-08-06 12:29:19 -0400

    Update patch_without_reboot.sh

--- patch_without_reboot.sh
+++ patch_without_reboot.sh
@@ -4,7 +4,7 @@ source wait_for_status.sh
  
 # update without reboot
  
-patch_command_id=$(aws ssm send-command --targets "Key=instanceIds,Values=i-069eb22f4a5273497,i-035f2c3dc689f04fc" --document-name "AWS-InstallWindowsUpdates" --comment "Install Windows updates without reboot" --parameters '{"Action":["Install"],"AllowReboot":["False"]}' --region $region)
+patch_command_id=$(aws ssm send-command --targets "Key=instanceIds,Values=i-069eb22f4a5273497,i-035f2c3dc689f04fc" --document-name "AWS-InstallWindowsUpdates" --comment "Install Windows updates without reboot" --parameters '{"Action":["Install"],"AllowReboot":["False"]}' --region $region | jq -r '.Command.CommandId')
  
 # Call the function from the source file
 waitforstatus Success $region $patch_command_id
