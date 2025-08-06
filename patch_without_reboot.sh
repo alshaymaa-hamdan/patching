1b4aa024e57bda7c98e032f18fadf9b2b131acbd
AlShaymaaHamdan <101719699+AlShaymaaHamdan@users.noreply.github.com>
2025-08-06 13:43:58 -0400

    Update patch_without_reboot.sh

--- patch_without_reboot.sh
+++ patch_without_reboot.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 region="us-west-2"
-source wait_for_status.sh
+
 instance_ids=$1
  waitforstatus () {
     target_status=$1
     target_region=$2
-    sh_command_id=$3
+
     ssmstatus="InProgress"
     while [ "$ssmstatus" != "$target_status" ]; do
-        ssmstatus=$(/usr/local/bin/aws ssm list-command-invocations --command-id $sh_command_id --details --query "CommandInvocations[*].StatusDetails[]" --output text)*
+        ssmstatus=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[*].StatusDetails[]" --output text)*
         sleep 5
         printf "Waiting for command to finish\n"
  
         if [ "$ssmstatus" == "Failed" ];
         then
-        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $sh_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)*
-        printf "\nCommand ID $sh_command_id has finished with following status: $ssmstatus\n"
+        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)*
+        printf "\nCommand ID $patch_command_id has finished with following status: $ssmstatus\n"
         printf "\nDestination output:----------------------------------------------\n"
         printf "\n$doutput\n"
         printf "\nEnd of output----------------------------------------------------\n"
         exit -1
         elif [ "$ssmstatus" == "Cancelled" ];
         then
-        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $sh_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)*
-        printf "\nCommand ID $sh_command_id has finished with following status: $ssmstatus\n"
+        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)*
+        printf "\nCommand ID $patch_command_id has finished with following status: $ssmstatus\n"
         printf "\nDestination output:----------------------------------------------\n"
         printf "\n$doutput\n"
         printf "\nEnd of output----------------------------------------------------\n"
         exit -1
         elif [ "$ssmstatus" == "DeliveryTimedOut" ];
         then
-        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $sh_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)
-        printf "\nCommand ID $sh_command_id has finished with following status: $ssmstatus\n"
+        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)
+        printf "\nCommand ID $patch_command_id has finished with following status: $ssmstatus\n"
         printf "\nDestination output:----------------------------------------------\n"
         printf "\n$doutput\n"
         printf "\nEnd of output----------------------------------------------------\n"
@@ -65,7 +65,7 @@ exit -1
 elif [ "$ssmstatus" == "InProgress" ];
 then
  
-waitforstatus Success $region $patch_command_id 
+waitforstatus Success $region
 fi
 
 doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)
