ef9a84cddbaae3f649a3840e8d6498122beb9cb6
AlShaymaaHamdan <101719699+AlShaymaaHamdan@users.noreply.github.com>
2025-08-06 13:46:11 -0400

    Update patch_without_reboot.sh

--- patch_without_reboot.sh
+++ patch_without_reboot.sh
@@ -2,77 +2,10 @@
 region="us-west-2"
 
 instance_ids=$1
- waitforstatus () {
-    target_status=$1
-    target_region=$2
 
-    ssmstatus="InProgress"
-    while [ "$ssmstatus" != "$target_status" ]; do
-        ssmstatus=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[*].StatusDetails[]" --output text)*
-        sleep 5
-        printf "Waiting for command to finish\n"
- 
-        if [ "$ssmstatus" == "Failed" ];
-        then
-        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)*
-        printf "\nCommand ID $patch_command_id has finished with following status: $ssmstatus\n"
-        printf "\nDestination output:----------------------------------------------\n"
-        printf "\n$doutput\n"
-        printf "\nEnd of output----------------------------------------------------\n"
-        exit -1
-        elif [ "$ssmstatus" == "Cancelled" ];
-        then
-        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)*
-        printf "\nCommand ID $patch_command_id has finished with following status: $ssmstatus\n"
-        printf "\nDestination output:----------------------------------------------\n"
-        printf "\n$doutput\n"
-        printf "\nEnd of output----------------------------------------------------\n"
-        exit -1
-        elif [ "$ssmstatus" == "DeliveryTimedOut" ];
-        then
-        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)
-        printf "\nCommand ID $patch_command_id has finished with following status: $ssmstatus\n"
-        printf "\nDestination output:----------------------------------------------\n"
-        printf "\n$doutput\n"
-        printf "\nEnd of output----------------------------------------------------\n"
-        exit -1
-        fi
-            done
-}
 # update without reboot
  
 patch_command_id=$(aws ssm send-command --targets "Key=instanceIds,Values=$instance_ids" --document-name "AWS-InstallWindowsUpdates" --comment "Install Windows updates without reboot" --parameters '{"Action":["Install"],"AllowReboot":["True"]}' --region $region | jq -r '.Command.CommandId')
 
-
 #i-069eb22f4a5273497,i-035f2c3dc689f04fc
 
-sleep 3
-ssmstatus=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[*].StatusDetails[]" --output text)
-sleep 3
- 
-echo "Command ID $patch_command_id is initiated"
-echo "Current command status: $ssmstatus "
- 
- 
-if [ "$ssmstatus" == "Failed" ];
-then
-doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)
-printf "\nCommand ID $patch_command_id has finished with following status: $ssmstatus\n"
-printf "\nDestination output:----------------------------------------------\n"
-printf "\n$doutput\n"
-printf "\nEnd of output----------------------------------------------------\n"
-exit -1
-elif [ "$ssmstatus" == "InProgress" ];
-then
- 
-waitforstatus Success $region
-fi
-
-doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)
-
-ssmstatus=$(/usr/local/bin/aws ssm list-command-invocations --command-id $patch_command_id --details --query "CommandInvocations[*].StatusDetails[]" --output text)
- 
-printf "\nCommand ID $patch_command_id has finished with $ssmstatus\n"
-printf "\nDestination output:----------------------------------------------\n"
-printf "\n$doutput\n"
-printf "\nEnd of output----------------------------------------------------\n"
