d3eb17625639604274228eb75cd3ca35a1f7cd4e
AlShaymaaHamdan <101719699+AlShaymaaHamdan@users.noreply.github.com>
2025-08-06 13:40:30 -0400

    Update patch_without_reboot.sh

--- patch_without_reboot.sh
+++ patch_without_reboot.sh
@@ -2,7 +2,43 @@
 region="us-west-2"
 source wait_for_status.sh
 instance_ids=$1
+ waitforstatus () {
+    target_status=$1
+    target_region=$2
+    sh_command_id=$3
+    ssmstatus="InProgress"
+    while [ "$ssmstatus" != "$target_status" ]; do
+        ssmstatus=$(/usr/local/bin/aws ssm list-command-invocations --command-id $sh_command_id --details --query "CommandInvocations[*].StatusDetails[]" --output text)*
+        sleep 5
+        printf "Waiting for command to finish\n"
  
+        if [ "$ssmstatus" == "Failed" ];
+        then
+        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $sh_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)*
+        printf "\nCommand ID $sh_command_id has finished with following status: $ssmstatus\n"
+        printf "\nDestination output:----------------------------------------------\n"
+        printf "\n$doutput\n"
+        printf "\nEnd of output----------------------------------------------------\n"
+        exit -1
+        elif [ "$ssmstatus" == "Cancelled" ];
+        then
+        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $sh_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)*
+        printf "\nCommand ID $sh_command_id has finished with following status: $ssmstatus\n"
+        printf "\nDestination output:----------------------------------------------\n"
+        printf "\n$doutput\n"
+        printf "\nEnd of output----------------------------------------------------\n"
+        exit -1
+        elif [ "$ssmstatus" == "DeliveryTimedOut" ];
+        then
+        doutput=$(/usr/local/bin/aws ssm list-command-invocations --command-id $sh_command_id --details --query "CommandInvocations[].CommandPlugins[*].[Output]" --output text)
+        printf "\nCommand ID $sh_command_id has finished with following status: $ssmstatus\n"
+        printf "\nDestination output:----------------------------------------------\n"
+        printf "\n$doutput\n"
+        printf "\nEnd of output----------------------------------------------------\n"
+        exit -1
+        fi
+            done
+}
 # update without reboot
  
 patch_command_id=$(aws ssm send-command --targets "Key=instanceIds,Values=$instance_ids" --document-name "AWS-InstallWindowsUpdates" --comment "Install Windows updates without reboot" --parameters '{"Action":["Install"],"AllowReboot":["True"]}' --region $region | jq -r '.Command.CommandId')
