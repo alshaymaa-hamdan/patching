e361d8a6fecbf7aa7969fb1eca2708f34903f1bf
AlShaymaaHamdan <101719699+AlShaymaaHamdan@users.noreply.github.com>
2025-08-08 14:07:05 -0400

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -2,7 +2,7 @@
  
 region="us-west-2"
 # source shared_jobs/wait_for_status.sh
-$instance_ids=$1
+instance_ids=$1
 reboot_command_id=$(aws ssm send-command --targets "Key=instanceIds,Values=$instance_ids" --document-name "AWS-RunPowerShellScript" --comment "Check for reboot and reboot if required after patching" --parameters 'commands=["$rebootPending = Test-Path \"HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WindowsUpdate\\Auto Update\\RebootRequired\"","if ($rebootPending) { Restart-Computer -Force }"]' --region $region)
  
 # Call the function from the source file
