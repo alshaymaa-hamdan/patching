93905ac2468627ece7a3857f3ca11b31e520fd9b
Aseel <109474697+aseelmalkawi@users.noreply.github.com>
2025-08-08 13:10:16 -0400

    Update stop_services.sh

--- stop_services.sh
+++ stop_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 region="us-west-2"
- 
+instance_ids=$1
 stop_command_id=$(aws ssm send-command --targets "Key=instanceIds,Values=$instances_ids" --document-name "AWS-RunPowerShellScript" --comment "Stopping Qlik services" --parameters '{"commands":["$services=@(\"Spooler\",\"W32Time\");foreach($svc in $services){try{$s=Get-Service -Name $svc -ErrorAction Stop;if($s.Status -ne \"Stopped\"){Stop-Service -Name $svc -Force -ErrorAction Stop;Write-Host \"Stopped $($s.DisplayName)\"}else{Write-Host \"$($s.DisplayName) already stopped\"}}catch{Write-Host \"Error with $svc: $_\"}}"]}' --region "$region")
  
 # Call the function from the source file
