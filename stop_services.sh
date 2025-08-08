da5ab5008aba6ec1a302aecbbf763f2813ba2b62
Aseel <109474697+aseelmalkawi@users.noreply.github.com>
2025-08-08 13:18:58 -0400

    Update stop_services.sh

--- stop_services.sh
+++ stop_services.sh
@@ -2,7 +2,7 @@
  
 region="us-west-2"
 instances_ids=$1
-stop_command_id=$(aws ssm send-command --targets "Key=instanceIds,Values=$instances_ids" --document-name "AWS-RunPowerShellScript" --comment "Stopping Qlik services" --parameters '{"commands":["$services=@(\"Spooler\",\"W32Time\");foreach($svc in $services){try{$s=Get-Service -Name $svc -ErrorAction Stop;if($s.Status -ne \"Stopped\"){Stop-Service -Name $svc -Force -ErrorAction Stop;Write-Host \"Stopped $($s.DisplayName)\"}else{Write-Host \"$($s.DisplayName) already stopped\"}}catch{Write-Host \"Error with $svc: $_\"}}"]}' --region "$region")
+stop_command_id=$(aws ssm send-command --targets "Key=instanceIds,Values=$instances_ids" --document-name "AWS-RunPowerShellScript" --comment "Stopping Qlik services" --parameters '{"commands":["$services=@(\"Spooler\",\"W32Time\");foreach($svc in $services){try{$s=Get-Service -Name $svc -ErrorAction Stop;if($s.Status -ne \"Stopped\"){Stop-Service -Name $svc -Force -ErrorAction Stop;Write-Host (\"Stopped \" + $s.DisplayName)}else{Write-Host ($s.DisplayName + \" already stopped\")}}catch{Write-Host (\"Error with \" + $svc + \": \" + $_)}}"]}' --region "$region")
  
 # Call the function from the source file
 # waitforstatus Success $region $stop_command_id
