91ff6503f5e33661f9ffc20de0b31b26b3b86d8a
Aseel <109474697+aseelmalkawi@users.noreply.github.com>
2025-08-11 15:54:57 -0400

    Update patching.sh

--- patching.sh
+++ patching.sh
@@ -4,4 +4,5 @@ region="us-west-2"
 sh_command_id=$(/usr/local/bin/aws ssm send-command --document-name "AWS-RunPatchBaseline" --instance-ids "$InstanceID"  --parameters '{"Operation":["Install"],"RebootOption":["RebootIfNeeded"]}' --timeout-seconds 600 --region $region --query "Command.CommandId" --output text)
 sleep 3
 echo "$sh_command_id"
+chmod +x wait_for_status.sh
 ./wait_for_status.sh $sh_command_id $region
