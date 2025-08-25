0dceb6de6815dcd54911f6213465b4cce2879782
Aseel <109474697+aseelmalkawi@users.noreply.github.com>
2025-08-25 11:16:08 -0400

    Update patch_without_reboot.py

--- patch_without_reboot.py
+++ patch_without_reboot.py
@@ -4,13 +4,14 @@ import os
 # Read environment variables
 instance_ids = os.getenv("InstanceID")  # expects comma-separated list
 region = "us-west-2"
- 
+instance_ids_list = [i.strip() for i in instance_id.split(",") if i.strip()]
+
 # Create SSM client
 ssm = boto3.client("ssm", region_name=region)
  
 # Send command
 response = ssm.send_command(
-    Targets=[{"Key": "instanceIds", "Values": instance_ids.split(",")}],
+    Targets=[{"Key": "instanceIds", "Values": instance_ids_list}],
     DocumentName="AWS-InstallWindowsUpdates",
     Comment="Install Windows updates without reboot",
     Parameters={
